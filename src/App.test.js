48152067741dcb4491b88077fdc0ada520ceb787
Slade Roberts <slade.a.roberts-1@ou.edu>
2026-01-29 16:53:29 -0600

    Deploy to GitHub Pages

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders Daria Shchukina heading', () => {
-  render(<App />);
-  const headingElement = screen.getByRole('heading', { name: /Daria Shchukina/i, level: 1 });
-  expect(headingElement).toBeInTheDocument();
-});
--- web/src/lib/image.ts
+++
@@ -1,4 +0,0 @@
-import createImageUrlBuilder from '@sanity/image-url'
-import { client } from './sanity'
-
-export const urlFor = (source: any) => createImageUrlBuilder(client).image(source)
